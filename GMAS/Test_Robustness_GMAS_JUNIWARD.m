24833df1aae2e818a66c49b98abe0625308a392c
everange <chencoriver@live.cn>
2020-12-23 16:38:16 +0800

    Update Test_Robustness_GMAS_JUNIWARD.m
    update the code comments

--- GMAS/Test_Robustness_GMAS_JUNIWARD.m
+++ GMAS/Test_Robustness_GMAS_JUNIWARD.m
@@ -1,18 +1,18 @@
 function Test_Robustness_GMAS_JUNIWARD()
-%% Â³°ôÒşĞ´Ëã·¨GMASÔÚÉç½»ÍøÂçÆ½Ì¨ Facebook Êµ¼ÊĞÅµÀÉÏµÄÂ³°ôĞÔ²âÊÔ´úÂë
+%% Robustness test code of the robust steganography algorithm GMAS on the actual channel of the social network platform Facebook
     clear all;
     clc;
-%%  ²ÎÊıÉèÖÃ
-    cover_dir = '.\cover_dir_QF65'; %ÔØÌåÍ¼ÏñËùÔÚÎÄ¼ş¼Ğ
-    stego_dir = '.\stego_dir'; if ~exist(stego_dir,'dir'); mkdir(stego_dir); end  %ÔØÃÜÍ¼ÏñËùÔÚÎÄ¼ş¼Ğ
-    afterchannel_stego_dir = '.\afterchannel_stego_dir'; if ~exist(afterchannel_stego_dir,'dir'); mkdir(afterchannel_stego_dir); end  %ĞÅµÀ´¦ÀíºóÔØÃÜÍ¼ÏñËùÔÚÎÄ¼ş¼Ğ     
-    cover_num = 10; %²âÊÔÔØÌåÍ¼Ïñ¸öÊı
-    cover_QF = 65; %ÔØÌåÍ¼ÏñµÄÖÊÁ¿Òò×Ó
-    Facebook_attack_QF = 71; %Ä£ÄâFacebookĞÅµÀÖÊÁ¿Òò×Ó£¬ÒòFacebook¶ÔÖÊÁ¿Òò×ÓQFµÍÓÚ71µÄÍ¼Ïñ¶¼½øĞĞQFÎª71µÄÑ¹Ëõ
-    payload = 0.1; %Ç¶ÈëÂÊ   
+%%  parameter setting
+    cover_dir = '.\cover_dir_QF65'; 
+    stego_dir = '.\stego_dir'; if ~exist(stego_dir,'dir'); mkdir(stego_dir); end  
+    afterchannel_stego_dir = '.\afterchannel_stego_dir'; if ~exist(afterchannel_stego_dir,'dir'); mkdir(afterchannel_stego_dir); end     
+    cover_num = 10; 
+    cover_QF = 65; 
+    Facebook_attack_QF = 71; 
+    payload = 0.1; 
     
-    bit_error_rate = zeros(1,cover_num); %¼ÇÂ¼²âÊÔÍ¼ÏñµÄÎóÂëÂÊ
-%%  ÏûÏ¢Ç¶Èë    
+    bit_error_rate = zeros(1,cover_num); 
+%%  message embedding   
     for i_img = 1:cover_num
         cover_Path = fullfile([cover_dir,'\',num2str(i_img),'.jpg']);   
         stego_Path = fullfile([stego_dir,'\',num2str(i_img),'.jpg']);    
@@ -20,46 +20,41 @@ function Test_Robustness_GMAS_JUNIWARD()
         
         C_STRUCT = jpeg_read(cover_Path);
         C_COEFFS = C_STRUCT.coef_arrays{1};  
-        C_QUANT = C_STRUCT.quant_tables{1}; %ÔØÌåÍ¼ÏñÁ¿»¯±í
+        C_QUANT = C_STRUCT.quant_tables{1};
         nzAC = nnz(C_COEFFS) - nnz(C_COEFFS(1:8:end,1:8:end));
-%  Ëæ»ú²úÉú¾ùÔÈ·Ö²¼µÄ¶ş½øÖÆÔ­Ê¼ÃØÃÜĞÅÏ¢£¬²¢½øĞĞ²¹Áã²Ù×÷
+%  generating random message and padding
         raw_msg_len = ceil(payload*nzAC);
-        raw_msg = round( rand(1,raw_msg_len) ); %Ô­Ê¼ÃØÃÜĞÅÏ¢µÄĞĞÏòÁ¿    
-        nn = 31; kk = 15; mm = 5;   %ÀûÓÃRS£¨31,15£©·Ö×é¾À´íÂëÊ±£¬ÃØÃÜĞÅÏ¢µÄ³¤¶ÈĞèÊÇ kk*mm=75 µÄÕûÊı±¶     
-        zeros_padding_num = ceil(raw_msg_len/kk/mm)*kk*mm - raw_msg_len; %ĞèÒª²¹ÁãµÄ¸öÊı
-        zeros_padding_msg = zeros(1, raw_msg_len + zeros_padding_num); %²»¹» kk*mm=75 µÄÕûÊı±¶£¬ºó±ß²¹Áã
+        raw_msg = round( rand(1,raw_msg_len) ); %original message    
+        nn = 31; kk = 15; mm = 5;   %using RSï¼ˆ31,15ï¼‰for message correctionï¼Œthe length of the message needs to be times of kk*mm=75      
+        zeros_padding_num = ceil(raw_msg_len/kk/mm)*kk*mm - raw_msg_len; %the number of padding zero 
+        zeros_padding_msg = zeros(1, raw_msg_len + zeros_padding_num); %padding
         zeros_padding_msg(1:raw_msg_len) = raw_msg;
-        zeros_padding_msg(raw_msg_len+1 : raw_msg_len + zeros_padding_num) = 0;  %²¹Áã²Ù×÷ºóµÃµ½µÄÃØÃÜĞÅÏ¢£¬¼´Êµ¼ÊÒªÇ¶ÈëµÄÃØÃÜĞÅÏ¢
-%  ÀûÓÃ RS£¨31,15£©¶ÔÃØÃÜĞÅÏ¢±àÂë        
+        zeros_padding_msg(raw_msg_len+1 : raw_msg_len + zeros_padding_num) = 0;  %message to be embedded subsequently
+%  The secret information is encoded using RS(31,15)    
         [rs_encoded_msg] = rs_encode_yxz(zeros_padding_msg,nn,kk); 
-%  ÀûÓÃ¸Ä½øµÄ·Ç¶Ô³ÆÊ§Õæ¿ò¼Ü¼ÆËãÔØÌåÔªËØµÄ +-1 ·Ç¶Ô³ÆÊ§Õæ
+%  An improved asymmetric distortion is used to calculate the +-1 asymmetric distortion of the cover 
         [rho1_P, rho1_M] = J_UNIWARD_Asy_cost(cover_Path);         
-%  ÏûÏ¢Ç¶ÈëÔ¤´¦Àí£¨¸ù¾İ¹ãÒå¶¶¶¯µ÷ÖÆ¼ÆËãĞŞ¸ÄÊ§ÕæºÍĞŞ¸Ä¾àÀë£©
+%  Message embedded preprocessing (modifying distortion and modifying distance based on generalized dither modulation)
         [cover_round, change_p, change_m, rho_p, rho_m] = gmas(cover_Path, rho1_P, rho1_M, C_QUANT);       
-%  ÀûÓÃÈıÔªSTC½øĞĞÏûÏ¢Ç¶Èë      
+%  STC embeding     
         [suc, stc_n_msg_bits] = stc3_embed(rs_encoded_msg, cover_Path, cover_round, rho_p, rho_m, change_p, change_m, cover_QF, stego_Path);
-%%  Ä£Äâ Facebook Ñ¹Ëõ  Facebook_attack_QF = 71   
+%%  mimic Facebook compression  Facebook_attack_QF = 71   
         imwrite(imread(stego_Path),afterchannel_stego_Path,'quality',Facebook_attack_QF);    
-%%  Êµ¼ÊµÄÉç½»ÍøÂçÆ½Ì¨FacebookÉÏ²âÊÔ
-%         breakpoint = 1; %³ÌĞòÔËĞĞÊ±ÔÚ´Ë´¦ÉèÖÃ¶Ïµã
-%         %½«ÔØÃÜÍ¼Ïñ stego_Path ÉÏ´«µ½Êµ¼ÊµÄÉç½»ÍøÂçÆ½Ì¨FacebookÉÏ£¬ÔÚ½øĞĞÏÂÔØ£¬ÏÂÔØºóµÄÍ¼ÏñÃüÃûÎª£ºafterchannel_stego_Path
-%         %¼ÌĞøÖ´ĞĞ³ÌĞò¼´¿É¡£        
-%%  ÏûÏ¢ÌáÈ¡
-%  ÀûÓÃÈıÔªSTC½øĞĞÏûÏ¢ÌáÈ¡
+
+%  STC extraction
         [stc_decoded_msg] = stc3_extract(afterchannel_stego_Path, stc_n_msg_bits, C_QUANT);   
-%  ÀûÓÃ RS£¨31,15£©¶ÔÃØÃÜĞÅÏ¢½âÂë         
+%  decode message by RSï¼ˆ31,15ï¼‰
         [rs_decoded_msg] = rs_decode_yxz(double(stc_decoded_msg), nn, kk);
-%  È¥µôÏûÏ¢Ä©Î²Ëù²¹µÄÁã        
-        extract_raw_msg = rs_decoded_msg(1:raw_msg_len); %È¥µô²¹Áã
-%%  ¼ÆËãÃ¿ÕÅÍ¼ÏñµÄÎóÂëÂÊ        
+%  delete padding       
+        extract_raw_msg = rs_decoded_msg(1:raw_msg_len); %delete padded zero
+%%  calculate bit error rate        
         bit_error = double(raw_msg) - double(extract_raw_msg);
         bit_error_number = sum(abs(bit_error));
-        bit_error_rate(1,i_img) = bit_error_number/raw_msg_len;
-%  Êä³öÃ¿ÕÅÍ¼ÏñµÄÎóÂëÂÊ               
+        bit_error_rate(1,i_img) = bit_error_number/raw_msg_len;              
         fprintf('%s\n',['payload: ',num2str(payload),'  image_number: ',num2str(i_img),'  error_rate: ',num2str(bit_error_rate(1,i_img))]);  
 
     end
-%  Êä³öËùÓĞÍ¼ÏñµÄÆ½¾ùÎóÂëÂÊ
+%  give it error rate of all images
     ave_error_rate = mean(bit_error_rate);
     fprintf('%s\n',['payload: ',num2str(payload),'  ave_error_rate: ',num2str(ave_error_rate)]);  
   
